b3ed4cb945e1cf5d6294d4184ed74783b980a90c
Edis MR <edisanthony@gmail.com>
2023-10-11 20:15:40 -0600

    cors for localhost

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { environment } from './environment/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+
+  /* cors for localhost */
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
+  
   await app.listen(environment.app_port);
 }
 bootstrap();
